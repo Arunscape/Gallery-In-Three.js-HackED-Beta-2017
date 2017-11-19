0fd96113af8b57812260d2d7b9b5fd03a563f73b
Tamara Bojovic <32472572+bojotamara@users.noreply.github.com>
2017-11-19 00:01:12 -0700

    idk tbh

--- testScript3.js
+++ testScript3.js
@@ -25,7 +25,7 @@ bulbMat = new THREE.MeshStandardMaterial( {
 bulbLight.add( new THREE.Mesh( bulbGeometry, bulbMat ) );
 bulbLight.position.set( 1, 2, 0 );
 bulbLight.castShadow = true;
-scene.add( bulbLight );
+//scene.add( bulbLight );
 
 bulbLight.shadow.mapSize.width = 512;  // default
 bulbLight.shadow.mapSize.height = 512; // default
@@ -61,15 +61,30 @@ camera.position.y = 0;
 camera.position.x = 0;
 
 var t = 0.001;
+var moving = false;
 
 //---------------------------------------------------------------
 
-
+/*
 var spotLight = new THREE.SpotLight(0xffffff,1,10);
-spotLight.position.set( 1, 2, 0 );
+camera.add(spotLight);
+spotLight.position.set( 0, 0, 1 );
 spotLight.castShadow = true;
-//spotLight.target = cube;
-scene.add( spotLight );
+spotLight.target = camera;
+scene.add(camera);
+//scene.add( spotLight );
+*/
+
+
+pointLight = new THREE.PointLight( 0xffffff );
+//pointLight.position.set(1,1,2);
+camera.add(pointLight);
+scene.add(camera);
+
+let helper = new THREE.CameraHelper(camera);
+scene.add(helper);
+
+
 
 
 
@@ -79,41 +94,48 @@ var animate = function () {
 	requestAnimationFrame( animate );
 
     camera.rotation.y = t;
-	spotLight.position.copy( camera.position );
-	spotLight.rotation.copy( camera.rotation );
+	//spotLight.position.copy( camera.position );
+	//spotLight.rotation.copy( camera.rotation );
+	//spotLight.target.updateMatrixWorld();
     renderer.render(scene, camera);
-<<<<<<< HEAD
 
-   
-=======
-    
-    
->>>>>>> Chris
 };
    
-    document.addEventListener('keydown', function(event) {
-        //Forward
-        if (event.keyCode == 38) {
-            camera.position.z+=0.08;
-            event.keyCode = 0;
-        }
-        //Backwards
-        if (event.keyCode == 40) {
-            camera.position.z-=0.08;
-            event.keyCode = 0;
-        }
-        //Left
-        if (event.keyCode == 37) {
-            t += 0.08;
-            //camera.rotation.y += 0.0001;
-        }
-        //Right
-        if (event.keyCode == 39) {
-            t -= 0.08;
-            //camera.rotation.y -= 0.0001;
-        }
-    }, true);
+document.addEventListener('keydown', function(event) {
+    //Forward
+    if (event.keyCode == 38) {
+        camera.position.z -= Math.cos(t)*0.10;
+        camera.position.x -= Math.sin(t)*0.10;
+        //event.keyCode = 0;
+        
+    }
+    //Backwards
+    if (event.keyCode == 40) {
+        camera.position.z += Math.cos(t)*0.10;
+        camera.position.x += Math.sin(t)*0.10;
+        //event.keyCode = 0;
+    }
+    //Left
+    if (event.keyCode == 37) {
+        t += 0.08;
+        //camera.rotation.y += 0.0001;
+    }
+    //Right
+    if (event.keyCode == 39) {
+        t -= 0.08;
+        //camera.rotation.y -= 0.0001;
+    }
+    //Bound the camera's z and x position to the shape of the
+    //cylinder.
+    typeof camera.position.z;
+    typeof camera.position.x;
+    
+    camera.position.z = Math.min(Math.max(camera.position.z,-9),9);
+    camera.position.x = Math.min(Math.max(camera.position.x,-9),9);
+    //Math.min(Math.max(number,1),20);
+}, true);
 //camera.lookAt(cylinder.position);
+
 animate();
 /*
  document.addEventListener('keydown', function(event) {
