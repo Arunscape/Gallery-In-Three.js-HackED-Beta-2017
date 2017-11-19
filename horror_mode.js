b0d763e02ede73131ca8f75a130233d5440ce5c5
Ya boi Chris <clo1@ualberta.ca>
2017-11-19 05:33:35 -0700

    Did something

--- horror_mode.js
+++ horror_mode.js
@@ -262,8 +262,8 @@ var audioLoader = new THREE.AudioLoader();
 //Load a sound and set it as the Audio object's buffer
 audioLoader.load( '/reee.mp3', function( buffer ) {
 	sound.setBuffer( buffer );
-	sound.setLoop( true );
-	sound.setVolume( 0.5 );
+	//sound.setLoop( true );
+	sound.setVolume( 1 );
 	sound.play();
 });
 
